edb1c3b16ff1fe7d8df3cfdd682aed9395e30cc9
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-13 18:40:26 +0200

    added file of main program

---
+++ fortun.f90
@@ -0,0 +1,18 @@
+!------------------------------------------------------------------------
+!
+!> Main program fortun.
+!
+!> fortun looks for files starting with 'test_' and compiles them if 
+!> needed, then it runs the test written in these files
+!
+!------------------------------------------------------------------------
+
+program fortun
+
+  
+
+
+
+
+
+end program fortun
