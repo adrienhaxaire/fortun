7d002a80375f6f4564d6c36c57919455464d2b20
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-15 21:09:38 +0200

    finding test files

--- fortun_generate.f90
+++ fortun_generate.f90
@@ -16,58 +16,79 @@ contains
   !--------------------------------------------------------------------- generate
   subroutine generate()
 
+    use fortun_utils, only : CHAR_LENGTH
+
     implicit none 
   
-    character(1024), dimension(:), allocatable :: test_files
+    character(CHAR_LENGTH), dimension(:), allocatable :: test_files
 
-    call find_tests(test_files)
+    call find_tests(".",test_files)
+
+    print *, trim(test_files(1))
 
   end subroutine generate
 
 
   !------------------------------------------------------------------- find_tests
   !> find the files which have a name starting with 'test_'
-  !! parses subdirectories as well
-  subroutine find_tests(test_files)
+  recursive subroutine find_tests(directory, test_files)
 
-    use fortun_utils, only : extend
+    use fortun_utils, only : extend, CHAR_LENGTH
 
     implicit none 
 
-    ! first and last columns of a 'ls -l' command
     integer, parameter :: io = 11
+    character(5), parameter :: test_prefix="test_"
+    integer, parameter :: NB_LS_COLUMNS = 8
+  
+    character(len=*), intent(IN) :: directory
+    character(len=*), dimension(:), allocatable, intent(INOUT) :: test_files
 
-    character(256), dimension(:) :: test_files
-    
+    character(CHAR_LENGTH) :: cmd
     character(1), dimension(:), allocatable :: first_column
-    character(256), dimension(:), allocatable ::  last_column
-    character(len=256), dimension(8) :: not_used
+    character(CHAR_LENGTH), dimension(:), allocatable ::  last_column
+    character(CHAR_LENGTH), dimension(NB_LS_COLUMNS) :: not_used
+    character(len(test_prefix)) :: file_prefix
     integer :: i, line, lines
 
-    call system("ls -l > list.txt")
-    
-    ! number of files, without the header "total <nb>"
+    if (directory .eq. "") then
+       return
+    else if (directory .eq. ".") then
+       cmd = "ls -l > list.txt"
+    else 
+       cmd = "ls -l "// trim(directory) //"> list.txt"
+    end if
+
+    call system(cmd)
+
+    ! number of lines minus the header "total <nb>" = nb of files/directories
     lines = number_of_lines("list.txt") - 1  
     allocate(first_column(lines), last_column(lines))    ! todo: handle alloc errors
 
-    ! get first and last column
     open(io, file="list.txt", form='formatted')
-    read(io,*)
+    read(io,*)  ! skip the header 
     do line=1,lines
        read(io,*) first_column(line), (not_used(i), i=1,6), last_column(line)
     end do
     close(io)
 
-    ! look in subdirectories, if any
-    do line=1,size(first_column)
-       if (first_column(line).eq."d") then
-          !call update_test_files(last_column(line))
-       end if
-    end do
-
     call system("rm list.txt")
 
+    do line=1,size(first_column)
+       if (first_column(line).eq."d") then
+          call find_tests(last_column(line), test_files)
+       else 
+          file_prefix = last_column(line)(1:len(test_prefix))
+          if (file_prefix .eq. test_prefix) then
+             call extend(test_files,1)
+             test_files(size(test_files)) = trim(directory)// "/" // last_column(line)
+          end if
+       end if
+    end do
+    
+
   end subroutine find_tests
+
 
 
   !-------------------------------------------------------------- number_of_lines
--- fortun_utils.f90
+++ fortun_utils.f90
@@ -5,6 +5,9 @@ module fortun_utils
   private
 
   public :: extend
+  public :: CHAR_LENGTH
+
+  integer, parameter :: CHAR_LENGTH = 256
 
   interface extend
      module procedure extend_char
@@ -21,21 +24,26 @@ contains
     character(len=*), dimension(:), allocatable, intent(IN OUT) :: array
     integer, intent(IN) :: extent
 
-    character(len=len(array)), dimension(:), allocatable :: tmp
+    character(len=CHAR_LENGTH), dimension(:), allocatable :: tmp
     integer :: length
     
-    length = size(array)
-    
-    allocate(tmp(length))
-    tmp = array
+    if (.not.allocated(array)) then
+       allocate(array(extent))
+       array = ""
+    else
+       length = size(array)
 
-    deallocate(array)
-    allocate(array(length+extent))
-    
-    array(1:length) = tmp
-    array(length+1:) = ""
-     
-    deallocate(tmp)
+       allocate(tmp(length))
+       tmp = array
+
+       deallocate(array)
+       allocate(array(length+extent))
+
+       array(1:length) = tmp
+       array(length+1:) = ""
+
+       deallocate(tmp)
+    end if
 
   end subroutine extend_char
 
