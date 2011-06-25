5e530116379da1d62b9e3cf156c70a0b5ff3f058
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-25 20:24:20 +0200

    added wrapper for modules that find files

---
+++ fortun_find.f90
@@ -0,0 +1,12 @@
+!--------------------------------------------------------------------------------
+!
+!> Wrapper module for find_tests and find_source
+!
+!--------------------------------------------------------------------------------
+module fortun_find
+
+  use fortun_find_sources
+
+  use fortun_find_tests
+
+end module fortun_find
