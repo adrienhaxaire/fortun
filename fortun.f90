a9244197ac47332b92cc5e1e5675566f8cf86985
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-25 20:28:46 +0200

    system call through string to handle other platforms

--- fortun.f90
+++ fortun.f90
@@ -15,21 +15,21 @@ program fortun
   implicit none 
 
   character(len=1024) :: cwd  !< current working directory
+  character(len=1024) :: test_cmd !< command to run the tests
 
   !-- process input: command line arguments, etc
-  if (.not.check_arguments()) then 
-     ! todo: display help/usage 
+  if (.not.check_arguments()) then   ! todo: display help/usage 
      print *, "pb arguments"
      stop
   end if
   
   !-- generation of test executable
   call get_cwd(cwd)
-  call generate(trim(cwd))
-
-
+  call generate(trim(cwd), "example")
+  
   !-- run all tests
-  ! call system("./fortun_test", stat)    !< only for linux now, should provide a better way
+  test_cmd = "./fortun_test"    !< only for linux now, should provide a better way
+  !call system(trim(cmd), stat)   
 
 
   !-- display results
