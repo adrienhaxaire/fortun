cd761f90adacceeed8e00c76e328de189eae7f06
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-26 19:00:28 +0200

    added other tests on my add + implicit none stmt

--- example/test_myadd.f90
+++ example/test_myadd.f90
@@ -1,14 +1,22 @@
 module test_myadd
 
-  use fortun_assertions
+  use fortun_assertions, only : assert_eq
 
 contains
 
-  logical function test_myadd_negative() result(res)
+  logical function test_myadd_dummy() result(res)
+    implicit none 
 
-    res = assert_eq(myadd(1+2),3)
+    integer :: myadd
+    logical :: plus, minus, zero
 
-  end function test_myadd_negative
+    plus = assert_eq(myadd(1,2),3)
+    minus = assert_eq(myadd(1,-2),-1)
+    zero = assert_eq(myadd(0,0),0)
+
+    res = plus .and. minus .and. zero
+    
+  end function test_myadd_dummy
 
 
 end module test_myadd
